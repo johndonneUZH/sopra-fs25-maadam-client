37e8ed35b3a01dc43aaaed0011e3e3c3963b7d2f
johndonneUZH <matteoiulian.adam@uzh.ch>
2025-02-21 08:58:08 +0100

    testing adding secret

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -5,9 +5,9 @@ import { isProduction } from "@/utils/environment";
  * In production it retrieves the URL from NEXT_PUBLIC_PROD_API_URL (or falls back to a hardcoded url).
  * In development, it returns "http://localhost:8080".
  */
-
+const NEXT_PUBLIC_PROD_API_URL = 'https://sopra-fs25-maadam-server.oa.r.appspot.com/';
 export function getApiDomain(): string {
-  const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
+  const prodUrl = NEXT_PUBLIC_PROD_API_URL ||
     "http://localhost:8080"; // TODO: update with your production URL as needed.
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
